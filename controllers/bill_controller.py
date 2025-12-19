d916c2836fbf217bdfeb44bcbd5f3f97e7fd59e6
GuevaraMansuino <gguevaraman@gmail.com>
2025-12-19 16:18:05 -0300

    bill controller para post

--- controllers/bill_controller.py
+++ controllers/bill_controller.py
@@ -1,6 +1,6 @@
 """Bill controller with proper dependency injection."""
 from controllers.base_controller_impl import BaseControllerImpl
-from schemas.bill_schema import BillSchema
+from schemas.bill_schema import BillSchema, BillCreateSchema
 from services.bill_service import BillService
 
 
@@ -10,6 +10,7 @@ class BillController(BaseControllerImpl):
     def __init__(self):
         super().__init__(
             schema=BillSchema,
+            create_schema=BillCreateSchema,
             service_factory=lambda db: BillService(db),
             tags=["Bills"]
         )
