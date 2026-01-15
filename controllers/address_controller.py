9c90ffee448447daf514a101d3848a6e04d53d61
GuevaraMansuino <gguevaraman@gmail.com>
2026-01-15 19:26:06 -0300

    address controller para no pedir id al front

--- controllers/address_controller.py
+++ controllers/address_controller.py
@@ -1,6 +1,6 @@
 """Address controller with proper dependency injection."""
 from controllers.base_controller_impl import BaseControllerImpl
-from schemas.address_schema import AddressSchema
+from schemas.address_schema import AddressSchema, AddressCreateSchema
 from services.address_service import AddressService
 
 
@@ -11,5 +11,6 @@ class AddressController(BaseControllerImpl):
         super().__init__(
             schema=AddressSchema,
             service_factory=lambda db: AddressService(db),
-            tags=["Addresses"]
+            tags=["Addresses"],
+            create_schema=AddressCreateSchema
         )
